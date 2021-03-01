566f946ae21a3fa9fece28ce68a44e8c95768eae
profelijah <elijahnyaroya@gmail.com>
2021-03-01 18:16:03 +0300

    My first library to be published

---
+++ toastlibrary/src/main/java/com/crypsol/toastlibrary/ToasterMessage.java
@@ -0,0 +1,11 @@
+package com.crypsol.toastlibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class ToasterMessage {
+
+    public void ShowMyToaster(Context cxt,String Message){
+        Toast.makeText(cxt, "elison : "+Message, Toast.LENGTH_SHORT).show();
+    }
+}
