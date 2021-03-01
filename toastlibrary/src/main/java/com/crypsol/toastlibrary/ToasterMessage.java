c36256973ac295669e04c1da49814ee975bb21f7
profelijah <elijahnyaroya@gmail.com>
2021-03-01 19:13:30 +0300

    "Second toaster message"

--- toastlibrary/src/main/java/com/crypsol/toastlibrary/ToasterMessage.java
+++ toastlibrary/src/main/java/com/crypsol/toastlibrary/ToasterMessage.java
@@ -3,7 +3,7 @@ package com.crypsol.toastlibrary;
 import android.content.Context;
 import android.widget.Toast;
 
-public class ToasterMessage {
+public   class ToasterMessage {
 
     public static void ShowMyToaster(Context cxt,String Message){
         Toast.makeText(cxt, "elison : "+Message, Toast.LENGTH_SHORT).show();
